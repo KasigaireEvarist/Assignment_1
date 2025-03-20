6a2e7e5877e6a69c64999b1d1b7821899f62cbdc
blac3pearl <umutoni12jos@gmail.com>
2025-03-20 14:23:36 +0300

    Average

--- main.c
+++ main.c
@@ -1,41 +1,56 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+
+int ass, coursework, midterm, endofterm;
+
+int math(){
+    printf("\t Math \n");
+    printf("Enter marks got the Math Assignment: ");
+    scanf("%d",&ass);
+    printf("Enter marks got the Math course work: ");
+    scanf("%d",&coursework);
+    printf("Enter marks got the Math midterm: ");
+    scanf("%d",&midterm);
+    printf("Enter marks got the Math end of term: ");
+    scanf("%d",&endofterm);
+
+    return (ass+coursework+midterm+endofterm)/4;
+}
+int chem(){
+    printf("\t Chemistry \n");
+    printf("Enter marks got the Chemistry Assignment: ");
+    scanf("%d",&ass);
+    printf("Enter marks got the Chemistry course work: ");
+    scanf("%d",&coursework);
+    printf("Enter marks got the Chemistry midterm: ");
+    scanf("%d",&midterm);
+    printf("Enter marks got the Chemistry end of term: ");
+    scanf("%d",&endofterm);
+
+    return (ass+coursework+midterm+endofterm)/4;
+}
+int phy(){
+    printf("\t Physics \n");
+    printf("Enter marks got the Physics Assignment: ");
+    scanf("%d",&ass);
+    printf("Enter marks got the Physics course work: ");
+    scanf("%d",&coursework);
+    printf("Enter marks got the Physics midterm: ");
+    scanf("%d",&midterm);
+    printf("Enter marks got the Physics end of term: ");
+    scanf("%d",&endofterm);
+
+    return (ass+coursework+midterm+endofterm)/4;
+}
 
 int main()
 {
-    int n,x;
-    printf("Enter the number of; \n");
-    printf("1.Domestic animals: ");
-    scanf("%d",&n);
-    printf("2.Wild animals: ");
-    scanf("%d",&x);
-
-    char domestic[n][50];
-    printf("Enter the Domestic animals: \n");
-    for(int i=0 ; i<n; i++){
-        printf("%d. ",i+1);
-        scanf("%s",&domestic[i]);
-    }
-    char wild[x][50];
-    printf("Enter the Wild animals: \n");
-    for(int i=0 ; i<x; i++){
-        printf("%d. ",i+1);
-        scanf("%s",&wild[i]);
-    }
-
-    char mergedarray[n+x][50];
-
-    for(int i = 0; i<n ;i++){
-        strcpy(mergedarray[i],domestic[i]);
-    }
-    for(int i = 0; i<x ;i++){
-        strcpy(mergedarray[n+i],wild[i]);
-    }
-
-    for(int i = 0; i<n+x ;i++){
-        printf("%s\t",mergedarray[i]);
-    }
+    int mathavg = math();
+    printf("Average mark is %d\n===============================================\n",mathavg);
+    int phyavg = phy();
+    printf("Average mark is %d\n===============================================\n",phyavg);
+    int chemavg = chem();
+    printf("Average mark is %d\n===============================================\n",chemavg);
 
     return 0;
 }
