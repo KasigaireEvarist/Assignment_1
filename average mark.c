61f00e459108efe176a17b822ff20919d8f03444
blac3pearl <umutoni12jos@gmail.com>
2025-03-20 14:53:56 +0300

    Update average mark.c

--- average mark.c
+++ average mark.c
@@ -1,35 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int ass, coursework, midterm, endofterm;
+double ass, coursework, midterm, endofterm;
 
-int math(){
+double math(){
     printf("\t Math \n");
     printf("Enter marks got the Math Assignment: ");
-    scanf("%d",&ass);
+    scanf("%lf",&ass);
     printf("Enter marks got the Math course work: ");
-    scanf("%d",&coursework);
+    scanf("%lf",&coursework);
     printf("Enter marks got the Math midterm: ");
-    scanf("%d",&midterm);
+    scanf("%lf",&midterm);
     printf("Enter marks got the Math end of term: ");
-    scanf("%d",&endofterm);
+    scanf("%lf",&endofterm);
 
     return (ass+coursework+midterm+endofterm)/4;
 }
-int chem(){
+double chem(){
     printf("\t Chemistry \n");
     printf("Enter marks got the Chemistry Assignment: ");
-    scanf("%d",&ass);
+    scanf("%lf",&ass);
     printf("Enter marks got the Chemistry course work: ");
-    scanf("%d",&coursework);
+    scanf("%lf",&coursework);
     printf("Enter marks got the Chemistry midterm: ");
-    scanf("%d",&midterm);
+    scanf("%lf",&midterm);
     printf("Enter marks got the Chemistry end of term: ");
-    scanf("%d",&endofterm);
+    scanf("%lf",&endofterm);
 
     return (ass+coursework+midterm+endofterm)/4;
 }
-int phy(){
+double phy(){
     printf("\t Physics \n");
     printf("Enter marks got the Physics Assignment: ");
     scanf("%d",&ass);
@@ -45,12 +45,14 @@ int phy(){
 
 int main()
 {
-    int mathavg = math();
-    printf("Average mark is %d\n===============================================\n",mathavg);
-    int phyavg = phy();
-    printf("Average mark is %d\n===============================================\n",phyavg);
-    int chemavg = chem();
-    printf("Average mark is %d\n===============================================\n",chemavg);
+    double mathavg = math();
+    printf("Average mark is %.1lf\n===============================================\n",mathavg);
+    double phyavg = phy();
+    printf("Average mark is %.1lf\n===============================================\n",phyavg);
+    double chemavg = chem();
+    printf("Average mark is %.1lf\n===============================================\n",chemavg);
+    double overallAverage = (phyavg+mathavg+chemavg)/3;
+    printf("The overall average in all subjects is %.2lf",overallAverage);
 
     return 0;
 }
