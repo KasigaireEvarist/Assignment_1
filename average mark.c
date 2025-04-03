d9eda3f881fcb22dde397debbd0fce980e8bc266
blac3pearl <umutoni12jos@gmail.com>
2025-04-03 15:40:13 +0300

    Update average mark.c

--- average mark.c
+++ average mark.c
@@ -1,90 +1,37 @@
 #include <stdio.h>
 #include <stdlib.h>
 char* exams[4]={"Assessment","Coursework","Midterm","End Of term"};
-
-double GetAvg_math(){
-
-    double math[4];
-    double sum1=0;
-
-    printf("Enter Math results:\n");
-    for(int i=0; i<4; i++){
-        while(1){
-        printf("%s:",exams[i]);
-
-        if(!scanf("%lf",&math[i])){
-            scanf("%*[^\n]");
+double GetAvg(char*subject){//average function with subjects as the parameter
+    double arr[4];//array to store user entries
+    double sum=0;//initialisation of the sum
+    printf("Enter %s results:\n",subject);//tells user to enter results
+    for(int i=0; i<4; i++){//loop to iterate through the array storing user entries
+        while(1){//checks the user input for validity
+        printf("%s:",exams[i]);//prints out what the user entered
+        if(!scanf("%lf",&arr[i])){
+            scanf("%*[^\n]");//frees space to enable new user entry
             printf("Invalid input\n");
-        }else if( math[i]>=0 &&math[i]<=100){
-                break;
+        }else if( arr[i]>=0 &&arr[i]<=100){//checks if input is between 0 and 100
+                break;//if it is right it breaks
             }else{
             printf("Entered Invalid Input.\n");
             }
         }
     }
-    for(int i=0; i<4; i++){
-        sum1=sum1+math[i];
+    for(int i=0; i<4; i++){//adds the users input stored in the array
+        sum=sum+arr[i];
     }
-    return sum1/4;
+    return sum/4;
 }
-double GetAvg_phy(){
-    double phy[4];
-    double sum2=0;
-    printf("Enter Physics results:\n");
-    for(int i=0; i<4; i++){
-        while(1){
-        printf("%s:",exams[i]);
-        if(!scanf("%lf",&phy[i])){
-            scanf("%*[^\n]");
-            printf("Invalid input\n");
-        }else if(phy[i]>=0 &&phy[i]<=100){
-                break;
-            }else{
-            printf("Entered Invalid Input.\n");
-            }
-        }
+int main(){
+    char subject[][10] = {"Math","Physics","Chemistry"};//array storing subjects
+    double avg,overallAvg = 0;
+    for(int i = 0; i<3; i++){
+        avg =GetAvg(subject[i]);
+        overallAvg =overallAvg+avg;
+        printf("\n%s avg: %.1lf%\n===============================\n",subject[i],avg);
     }
-    for(int i=0; i<4; i++){
-        sum2=sum2+phy[i];
-    }
-    return sum2/4;
-}
-double GetAvg_chem(){
-    double chem[4];
-    double sum3=0;
-    printf("Enter Chem results:\n");
-    for(int i=0; i<4; i++){
-        while(1){
-        printf("%s: ",exams[i]);
-        if(!scanf("%lf",&chem[i])){
-            scanf("%*[^\n]");
-            printf("Invalid input\n");
-        }else if(chem[i]>=0 &&chem[i]<=100){
-                break;
-            }else{
-            printf("Entered Invalid Input.\n");
-            }
-        }
-    }
-    for(int i=0; i<4; i++){
-        sum3=sum3+chem[i];
-    }
-    return sum3/4;
-}
-
-int main()
-{
-    double avg_math = GetAvg_math();
-    printf("\nMath avg: %.1lf%\n===============================\n",avg_math);
-
-    double avg_phy = GetAvg_phy();
-    printf("\nPhysics avg: %.1lf%\n===============================\n",avg_phy);
-
-    double avg_chem = GetAvg_chem();
-    printf("\nChemistry avg: %.1lf%\n===============================\n",avg_chem);
-
-    double overall_avg = (avg_math+avg_phy+avg_chem)/3;
+    double overall_avg =overallAvg /3;
     printf("\nThe overall average: %.2lf%\n===============================\n",overall_avg);
-
     return 0;
 }
